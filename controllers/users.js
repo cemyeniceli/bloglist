c5f1f24894a622131593572b553c1fa73243cead
cemyeniceli <cemyeniceli@gmail.com>
2022-01-21 16:05:22 +0100

    added search by username

--- controllers/users.js
+++ controllers/users.js
@@ -7,6 +7,15 @@ usersRouter.get('/', async (request, response) => {
 	response.json(users)
 })
 
+usersRouter.get('/:username', async (request, response) => {
+	const user = await User.findOne({username:request.params.username})
+	if (user) {
+		response.json(user)
+	} else {
+		response.status(404).end()
+	}
+})
+
 usersRouter.post('/', async (request, response) => {
 	const body = request.body
 	if (body.password.length < 3) {
