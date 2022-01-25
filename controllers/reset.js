bd06071243cff258fc5653206b24a9ff22c2d69a
cemyeniceli <cemyeniceli@gmail.com>
2022-01-25 11:41:25 +0100

    added testRouter for reseting the db

---
+++ controllers/reset.js
@@ -0,0 +1,11 @@
+const testRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+testRouter.post('/reset', async (request, response) => {
+    await Note.deleteMany({})
+    await User.deleteMany({})
+    response.status(204).end()
+})
+  
+  module.exports = testRouter
