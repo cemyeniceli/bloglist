875eea66665e8d5465252326711666246738a7de
cemyeniceli <cemyeniceli@gmail.com>
2022-01-31 11:33:32 +0100

    typo fix

--- controllers/reset.js
+++ controllers/reset.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 testRouter.post('/reset', async (request, response) => {
-    await Note.deleteMany({})
+    await Blog.deleteMany({})
     await User.deleteMany({})
     response.status(204).end()
 })
